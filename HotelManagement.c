50e4536f2d5e91f0fb800d5cc5dc311ea04a43cd
ZONEboyff <117200591+ZONEboyff@users.noreply.github.com>
2023-07-24 22:18:24 +0530

    Update HotelManagement.c

--- HotelManagement.c
+++ HotelManagement.c
@@ -1,23 +1,28 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include"task.h"
+#include <stdio.h>//standard library header file
+#include <string.h>//string handling header file
+#include <stdlib.h>//standard library header file to use DMA
+#include <unistd.h>//to use the access fucntion
+#include <windows.h>//to use system("cls")
+#include "task.h"//custom header files containing functions to execute this program
 int main() {
     struct customer* c;
     FILE* fp;
     int choice;
     while (1) {
-        int flag=0;
-        printf("Enter the NUMBER to perform the specific task:\n");
-        printf("(1)--> TO ADD NEW CUSTOMER RECORD\n");
-        printf("(2)--> TO UPDATE AGE OF ANY CUSTOMER RECORD\n");
-        printf("(3)--> TO DISPLAY ANY CUSTOMER RECORD\n");
-        printf("(4)--> CUSTOMER FEEDBACK\n");
-        printf("(5)--> END\n");
+        int flag = 0;//Used for ending the manu
+        printf("*************************************************************\n");
+        printf("*  Enter the NUMBER to perform the specific task:           *\n");
+        printf("*  (1)--> TO ADD NEW CUSTOMER RECORD                        *\n");
+        printf("*  (2)--> TO UPDATE AGE OF ANY CUSTOMER RECORD              *\n");
+        printf("*  (3)--> TO DISPLAY ANY CUSTOMER RECORD                    *\n");
+        printf("*  (4)--> CHECKING OUT                                      *\n");
+        printf("*  (5)--> CUSTOMER FEEDBACK                                 *\n");
+        printf("*  (6)--> END                                               *\n");
+        printf("*************************************************************\n");
         printf("Choice: ");
         scanf("%d", &choice);
-        switch(choice){
+        
+        switch (choice) {
             case 1:
                 addcust(fp);
                 break;
@@ -29,21 +34,28 @@ int main() {
             case 3:
                 display(fp);
                 break;
-
+            
             case 4:
-                custfeed(fp);
+                checkout(fp);
                 break;
 
             case 5:
-                flag=1;
+                custfeed(fp);
+                break;
+
+            case 6:
+                flag = 1;
                 break;
 
             default:
                 printf("Invalid choice. Please try again.\n");
-        }if(flag==1){
-            printf("End of Program...");
+        }
+        
+        if (flag == 1) {//checks if flag==1 which is only possible when user selects (6) or end
+            printf("End of Program...\n");
             break;
         }
     }
+    
     return 0;
 }
